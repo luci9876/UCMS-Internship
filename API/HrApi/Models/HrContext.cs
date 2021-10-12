d43544fa266961ba52b60e02a0f11578f901f558
luci9876 <91459388+luci9876@users.noreply.github.com>
2021-10-12 19:11:37 +0300

    api

---
+++ API/HrApi/Models/HrContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HrApi.Models
+{
+    public class HrContext : DbContext
+    {
+        public HrContext(DbContextOptions<HrContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
