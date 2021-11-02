f39a76148eeaa13e3104308153ee7a3d7781a1ea
luci9876 <91459388+luci9876@users.noreply.github.com>
2021-11-02 11:31:24 +0200

    refactoring companies & employees pages

--- UI/Hr-App-UI/Hr-App-UI/src/app/companies/companies.component.ts
+++ UI/Hr-App-UI/Hr-App-UI/src/app/companies/companies.component.ts
@@ -4,6 +4,8 @@ import { Company } from '../models/company';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ImagesServiceService } from '../services/images-service.service';
 import { Image } from '../models/image';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-companies',
@@ -15,26 +17,28 @@ export class CompaniesComponent implements OnInit {
   public currentCompany:Company;
   public currentId:number=0;
   public base64Image: SafeUrl;
-  public domSanitizer:DomSanitizer;
+  
   public imageService: ImagesServiceService;
   public currentImage:Image;
+  private baseUrl: string = `${environment.apiUrl}Image`;
+  public fileName:string;
+  private selectedFile: File;
   
-  constructor(private employeesService: CompanyService) { }
+  
+  constructor(private employeesService: CompanyService,public domSanitizer:DomSanitizer,private http: HttpClient) {
+    this.fileName="No Input Chosen";
+   }
 
   ngOnInit(): void {
-    this.loadCompanies();
-    console.log(this.companies);
-    console.log("here");
     this.currentImage= {id:0,imageTitle:" ",imageData:" " };
     this.currentCompany= {id:0,name:" ",description:" ",image:this.currentImage,founded:1900 };
-    
+    this.base64Image=this.domSanitizer.bypassSecurityTrustUrl('../../assets/default-photo/logo.png');
+    this.loadCompanies();
     
   }
   private loadCompanies() {
     this.employeesService.getCompanies().subscribe((companies) => {
-      this.companies = companies;
-      console.log(this.companies);
-      
+      this.companies = companies; 
     })
   }
   public loadCompany() {
@@ -69,6 +73,36 @@ export class CompaniesComponent implements OnInit {
       this.currentImage = image,
       this.base64Image =this.domSanitizer.bypassSecurityTrustUrl('data:image/png;base64,'+this.currentImage.imageData);
     })
+  }
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    this.fileName=this.selectedFile.name;
+  }
+  Upload() {
+    var index = this.fileName.lastIndexOf('.');
+    var currentExtension = this.fileName.substring(index + 1).trim();
+    let allExtensions: Array<string> = ["png", "jpg", "jpeg", "gif", "tiff", "bpg"];
+    var exists:boolean=false;
+    for(let element of allExtensions)
+    {
+      if(currentExtension===element)
+       {
+         exists=true;
+        break;}
+    }
+    if(exists)
+    {
+    const uploadFile = new FormData();
+    uploadFile.append('myFile', this.selectedFile, this.selectedFile.name);
+    this.http.post(this.baseUrl,uploadFile,)
+      .subscribe(res=>{
+        console.log(res);
+      });
+    }
+      else
+      {
+       alert("This file is not an image!");
+      }
   }
   
 
--- UI/Hr-App-UI/Hr-App-UI/src/app/employees/employees.component.ts
+++ UI/Hr-App-UI/Hr-App-UI/src/app/employees/employees.component.ts
@@ -5,6 +5,9 @@ import { EmployeeCardComponent } from '../employee-card/employee-card.component'
 import { Image } from '../models/image';
 import { SafeUrl,DomSanitizer } from '@angular/platform-browser';
 import { ImagesServiceService } from '../services/images-service.service';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
@@ -19,14 +22,20 @@ export class EmployeesComponent implements OnInit {
   public currentImage:Image;
   public currentId:number=0;
   public base64Image: SafeUrl;
-  public domSanitizer:DomSanitizer;
   public imageService: ImagesServiceService;
+  private baseUrl: string = `${environment.apiUrl}Image`;
+  public fileName:string;
+  private selectedFile: File;
   
-  constructor(private employeesService: EmployeeService) { }
-
+  constructor(private employeesService: EmployeeService, public domSanitizer:DomSanitizer,private http: HttpClient) 
+  { 
+  this.fileName="No Input Chosen";
+  }
   ngOnInit(): void {
     this.currentImage= {id:0,imageTitle:" ",imageData:" " };
     this.currentEmployee= {id:0,firstName:" ",lastName:" ",email:" " ,image:this.currentImage};
+    this.base64Image=this.domSanitizer.bypassSecurityTrustUrl('../../assets/default-photo/default.png');
+    
     this.loadEmployees();
     
   }
@@ -67,6 +76,36 @@ export class EmployeesComponent implements OnInit {
       this.currentImage = image,
       this.base64Image =this.domSanitizer.bypassSecurityTrustUrl('data:image/png;base64,'+this.currentImage.imageData);
     })
+  }
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    this.fileName=this.selectedFile.name;
+  }
+  Upload() {
+    var index = this.fileName.lastIndexOf('.');
+    var currentExtension = this.fileName.substring(index + 1).trim();
+    let allExtensions: Array<string> = ["png", "jpg", "jpeg", "gif", "tiff", "bpg"];
+    var exists:boolean=false;
+    for(let element of allExtensions)
+    {
+      if(currentExtension===element)
+       {
+         exists=true;
+        break;}
+    }
+    if(exists)
+    {
+    const uploadFile = new FormData();
+    uploadFile.append('myFile', this.selectedFile, this.selectedFile.name);
+    this.http.post(this.baseUrl,uploadFile,)
+      .subscribe(res=>{
+        console.log(res);
+      });
+    }
+      else
+      {
+       alert("This file is not an image!");
+      }
   }
   
 
